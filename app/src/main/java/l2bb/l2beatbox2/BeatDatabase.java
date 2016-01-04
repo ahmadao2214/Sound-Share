0b70a027b8d727f682103397149bc19d613ce415
Ozzie Ahmad <ahmad_ao2214@hotmail.com>
2016-01-03 21:40:55 -0600

    Added insertData method to BeatsDatabase class

--- app/src/main/java/l2bb/l2beatbox2/BeatDatabase.java
+++ app/src/main/java/l2bb/l2beatbox2/BeatDatabase.java
@@ -1,5 +1,6 @@
 package l2bb.l2beatbox2;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -20,7 +21,6 @@ public class BeatDatabase extends SQLiteOpenHelper{
 
     public BeatDatabase(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
@@ -33,5 +33,20 @@ public class BeatDatabase extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name, String type){
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,type);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1){
+            return false;
+        }
+        else
+            return true;
     }
 }
